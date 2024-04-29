4e12fe99c8fef5424e5c34a40a3c65633e6f1c1f
vongvoro <vongvoro@daou.co.kr>
2024-04-29 13:13:09 +0900

    Chore: 외부 디펜던시 version 가져올 때 upToNextMinor로 가져오도록 변경

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/DaouMobile/DaouGeometry2D", exact: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.2.0")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxGesture", .upToNextMajor(from: "4.0.1")),
-        .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.0"))
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMinor(from: "6.2.0")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxGesture", .upToNextMinor(from: "4.0.1")),
+        .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMinor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
