02ab3a2c1a376d74889555f11d6b35cba71e1e9c
vongvoro <vongvoro@daou.co.kr>
2024-04-29 10:17:30 +0900

    Chore: 외부 라이브러리 의존성 dependency에서 제거, target에서만 유지

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             targets: ["DaouEnfaxImageEditor"])
     ],
     dependencies: [
-        .package(url: "https://github.com/DaouMobile/DaouGeometry2D", branch: "main"),
-        .package(url: "https://github.com/ReactiveX/RxSwift", exact: "6.2.0"),
-        .package(url: "https://github.com/RxSwiftCommunity/RxGesture", exact: "4.0.4"),
-        .package(url: "https://github.com/SnapKit/SnapKit", exact: "5.0.1")
+        .package(url: "https://github.com/DaouMobile/DaouGeometry2D", branch: "main")
+//        .package(url: "https://github.com/ReactiveX/RxSwift", exact: "6.2.0"),
+//        .package(url: "https://github.com/RxSwiftCommunity/RxGesture", exact: "4.0.4"),
+//        .package(url: "https://github.com/SnapKit/SnapKit", exact: "5.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
